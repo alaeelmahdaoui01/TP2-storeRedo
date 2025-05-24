009d62c6e171c5a986bb4d65389f1cc17c07e0ed
El Mahdaoui Alae <alaeelmahdaoui01@gmail.com>
2025-05-24 22:52:08 +0100

    start

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<!-- the default layout for all pages -->
+
+<template>
+    <div>
+        <nav>
+            <div to="/">FakeStore</div>
+            <div to="/">Home</div>
+            <div to="/About">About</div>
+            <div to="/Products/">Products</div>
+        </nav>
+
+
+        <main>
+        
+        </main>
+
+
+        <footer>
+        
+        </footer>
+    </div>
+</template>
